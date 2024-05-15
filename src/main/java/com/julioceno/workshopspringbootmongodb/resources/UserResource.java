bbbb59878bf97e99748010ac61bea19274483430
Júlio Nepomuceno <julioreplay@gmail.com>
2024-05-14 22:47:19 -0300

    [feature]: update user

--- src/main/java/com/julioceno/workshopspringbootmongodb/resources/UserResource.java
+++ src/main/java/com/julioceno/workshopspringbootmongodb/resources/UserResource.java
@@ -58,6 +58,14 @@ public class UserResource {
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable String id) {
         userService.delete(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = userService.fromDTO(objDto);
+        obj.setId(id);
+        obj = userService.update(obj);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/julioceno/workshopspringbootmongodb/services/UserService.java
+++ src/main/java/com/julioceno/workshopspringbootmongodb/services/UserService.java
@@ -37,6 +37,18 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
+    public User update(User user) {
+        User newUser = findById(user.getId());
+        updateData(newUser, user);
+        return userRepository.save(newUser);
+    }
+
+    private User updateData(User newUser, User user) {
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+        return newUser;
+    }
+
     public User fromDTO(UserDTO userDto) {
         return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
     }
