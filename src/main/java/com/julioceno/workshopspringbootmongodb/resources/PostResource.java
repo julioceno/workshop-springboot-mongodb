f113282749fcaafb94ef2868ffd993bef9aade70
Júlio Nepomuceno <julioreplay@gmail.com>
2024-05-18 13:32:28 -0300

    [feature]: create query

--- src/main/java/com/julioceno/workshopspringbootmongodb/resources/PostResource.java
+++ src/main/java/com/julioceno/workshopspringbootmongodb/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.julioceno.workshopspringbootmongodb.resources;
 import com.julioceno.workshopspringbootmongodb.domain.Post;
 import com.julioceno.workshopspringbootmongodb.domain.User;
 import com.julioceno.workshopspringbootmongodb.dto.UserDTO;
+import com.julioceno.workshopspringbootmongodb.resources.util.URL;
 import com.julioceno.workshopspringbootmongodb.services.PostService;
 import com.julioceno.workshopspringbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/julioceno/workshopspringbootmongodb/services/PostService.java
+++ src/main/java/com/julioceno/workshopspringbootmongodb/services/PostService.java
@@ -26,5 +26,7 @@ public class PostService {
         return post.get();
     }
 
-
+    public List<Post> findByTitle(String text) {
+        return postRepository.searchTitle(text);
+    }
 }
